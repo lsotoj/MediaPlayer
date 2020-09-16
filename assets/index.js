51f46e6fd3e87d17cf17aff54e0bd0c0a9dd08ef
lsotoj <luissotoj@gmail.com>
2020-09-16 15:58:16 -0600

    Para mejorar la experiencia del usuario, al autoplay se le quitó el audio. En este commit agregúe el boton para quitarle el mute al video

--- assets/index.js
+++ assets/index.js
@@ -5,5 +5,7 @@ import AutoPlay from './plugins/AutoPlay.js';
 const video = document.querySelector("video");
 const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
 
-const button = document.querySelector("button");
+const button = document.querySelector(".playPause");
+const mute = document.querySelector(".mute");
 button.onclick = () => player.tooglePlay();
+mute.onclick = () => console.log("muted");
