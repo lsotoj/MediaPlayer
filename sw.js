c06001e20452b9acc30460489bd1823a2a542e6e
lsotoj <luissotoj@gmail.com>
2020-10-13 20:49:51 -0600

    He finalizado la implementacion de service workers, y el contenido lo toma de chache y tambien actualiza a travez de la red para no mantener contenido no actualizado.

--- sw.js
+++ sw.js
@@ -1,17 +1,43 @@
+const VERSION = "v1";
 self.addEventListener('install', event => {
     event.waitUntil(precache());
 });
 
+self.addEventListener('fetch', event => {
+    const request = event.request;
+    if(request.method !==  "GET") {
+        return;
+    }
+
+    //buscar en cache
+    event.respondWith(cachedResponse(request))
+
+    // actualizar el cache
+    event.waitUntil(updateCache(request));
+})
 async function precache () {
-   const cache = await caches.open('v1');
-  
-   return cache.addAll([
+    const cache = await caches.open(VERSION);
+    return cache.addAll([
        '/',
        '/index.html',
        '/assets/MediaPlayer.js',
        '/assets/plugins/AutoPlay.js',
        '/assets/plugins/AutoPause.js',
        '/assets/index.js',
-       '/assets/ajercicio.mp4',
-   ]);
+       '/assets/ejercicio.mp4',
+     ]);
 }
+
+async function cachedResponse(request) {
+    const cache = await caches.open(VERSION);
+    const response = await cache.match(request);
+    return response || fetch(request);
+}
+
+async function updateCache(request) {
+    const cache = await caches.open(VERSION);
+    const response = await fetch(request);
+    return cache.put(request, response);
+}
+
+
